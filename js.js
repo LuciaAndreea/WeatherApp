42fede11eeb9c4d887fb152e2a139dec33529912
Lucia <luciaandreeaursu@yahoo.com>
2025-01-07 14:53:39 +0200

    fetching weather details from the API for the entered city

--- js.js
+++ js.js
@@ -1,4 +1,18 @@
 const searchInput = document.querySelector(".search-input");
+const API_KEY = "79493d26b5364e3fb1695446242211";
+
+const getWeatherDetails = async (cityName) =>{
+    const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}`;
+
+    try{
+      const response = await fetch(API_URL);
+      const data = await response.json();
+
+      console.log(data);
+    } catch(error){
+        console.log(error);
+    }
+}
 
 //show the city on console when enter is clicked
 searchInput.addEventListener("keyup" , (e) =>{
@@ -6,6 +20,6 @@ searchInput.addEventListener("keyup" , (e) =>{
     const cityName = searchInput.value.trim();
 
     if(e.key == "Enter" && cityName){
-        console.log(cityName);
+        getWeatherDetails(cityName);
     }
 })
