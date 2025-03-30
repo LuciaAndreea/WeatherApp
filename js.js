93f9be1d929cfa408304c8c84a18857e61f7f792
Lucia <luciaandreeaursu@yahoo.com>
2025-03-30 13:20:31 +0300

    fix deployment

--- js.js
+++ js.js
@@ -69,7 +69,7 @@ const getWeatherDetails = async (API_URL) =>{
 }
 
 const setupWeatherRequest = (cityName) =>{
-    const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}&days=2`;
+    const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}&days=2`;
     getWeatherDetails(API_URL);
 }
 
@@ -87,7 +87,7 @@ searchInput.addEventListener("keyup" , (e) =>{
 locationButton.addEventListener("click", () =>{
     navigator.geolocation.getCurrentPosition(position =>{
         const {latitude, longitude} = position.coords;
-        const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${latitude},${longitude}&days=2`;
+        const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${latitude},${longitude}&days=2`;
         getWeatherDetails(API_URL);
     }, error =>{
         alert("Location acces denied. Please enable permissions to use this feature.")
